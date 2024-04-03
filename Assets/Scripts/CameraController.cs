59876a7dac644cf01390e7ff351255d6bdb2eeec
rkidderFSU <rkidder@student.fitchburgstate.edu>
2024-04-03 02:33:39 -0400

    Shop Decoration + Inspectable Chips
    Added some decoration to the shop including a door (wow!). Also added the bag of chips and a script for shop items.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     public Transform player;
     public float smoothSpeed = 1f; //how "lazy" the camera is
     public Vector3 offset; //distance between the camera and the player
+    public float leftBoundary = 12f;
+    public float rightBoundary;
     
     void Start()
     {
@@ -20,6 +22,14 @@ public class CameraController : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); //smoothly move the camera to the desired position
 
         smoothedPosition.z = transform.position.z;
-        transform.position = smoothedPosition;
+       /* if (player.transform.position.x < leftBoundary)
+        {
+            transform.position = new Vector3(3, smoothedPosition.y, smoothedPosition.z);
+        }
+        else
+        {
+            transform.position = smoothedPosition;
+        } */
+       transform.position = smoothedPosition;
     }
 }
---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Color idleColor = new Color(0.75f, 0.75f, 0.75f);
+    public GameObject infoPopup;
+    public bool inCart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = idleColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        // Highlight the item and show info text
+        sr.color = Color.white;
+        infoPopup.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        // Hide info text and revert color
+        sr.color = idleColor;
+        infoPopup.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        // Add the item to the "cart"
+        gameObject.SetActive(false);
+        inCart = true; // This variable will come into play when we add customers
+    }
+}
