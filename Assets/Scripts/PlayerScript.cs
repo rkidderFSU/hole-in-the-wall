c1e64678e3dfacbff1cfc01b99b0f09b8cd25b41
rkidderFSU <rkidder@student.fitchburgstate.edu>
2024-04-09 18:23:43 -0400

    More Items Added
    D20, Battery, Bottle Cap, Chess Pawn, Fidget Spinner, Marble, Pencil Sharpener, Stamp, Domino, D6. Also added the shop theme.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,11 +7,15 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rb;
     public float speed = 10f;
     public float maxSpeed = 10f;
+    private bool movingLeft;
+    private bool movingRight;
+    private SpriteRenderer sr;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
@@ -29,5 +33,32 @@ public class PlayerScript : MonoBehaviour
             rb.AddForce(movement * speed);
         }
 
+    }
+
+    private void Update()
+    {
+        CheckMovementDirection();
+    }
+
+    private void CheckMovementDirection()
+    {
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !movingRight)
+        {
+            movingLeft = true;
+            sr.flipX = true;
+        }
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !movingLeft)
+        {
+            movingRight = true;
+            sr.flipX = false;
+        }
+        if ((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) && !movingRight)
+        {
+            movingLeft = false;
+        }
+        if ((Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) && !movingLeft)
+        {
+            movingRight = false;
+        } 
     }
 }
